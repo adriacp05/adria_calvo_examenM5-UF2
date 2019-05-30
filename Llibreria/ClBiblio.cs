222598a5215de96d214871ebddf901a663eaacca
adriacp05 <adriacp05@hotmail.es>
2019-05-30 09:13:08 +0200

    Versio 1

---
+++ Llibreria/ClBiblio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Llibreria
+{
+    public class ClBiblio
+    {
+        public Boolean esPrimer(Int32 num)
+        {
+            int i = 2;
+            Boolean trobat = false;
+            Boolean xtrue = false;
+
+            while (i < num && trobat == false)
+            {
+                if (num % i == 0)
+                {
+                    xtrue = true;
+                    trobat = true;
+                }
+                i++;
+            }
+            
+            return xtrue;
+        }
+    }
+}
---
+++ Proves/Proves.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Llibreria;
+
+namespace Proves
+{
+    [TestClass]
+    public class Proves
+    {
+        [TestMethod]
+        public void primer1()
+        {
+            Boolean resultat;
+            ClBiblio clE = new ClBiblio();
+            resultat = clE.esPrimer(7);
+            //Console.WriteLine("Resultado: " + resultat);
+            Assert.AreEqual(false, resultat);
+        }
+
+        [TestMethod]
+        public void primer2()
+        {
+            Boolean resultat;
+            ClBiblio clE = new ClBiblio();
+            resultat = clE.esPrimer(5);
+            //Console.WriteLine("Resultado: " + resultat);
+            Assert.AreEqual(false, resultat);
+        }
+
+        [TestMethod]
+        public void primer3()
+        {
+            Boolean resultat;
+            ClBiblio clE = new ClBiblio();
+            resultat = clE.esPrimer(8);
+            //Console.WriteLine("Resultado: " + resultat);
+            Assert.AreEqual(true, resultat);
+        }
+
+        [TestMethod]
+        public void primer4()
+        {
+            Boolean resultat;
+            ClBiblio clE = new ClBiblio();
+            resultat = clE.esPrimer(3);
+            //Console.WriteLine("Resultado: " + resultat);
+            Assert.AreEqual(false, resultat);
+        }
+
+    }
+}
